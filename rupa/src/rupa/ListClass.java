65b4f566b97dda2e84c3bf946695aacb067e3d3a
GuvalaL2 <GuvalaL2@10.242.6.58>
2016-12-21 16:12:33 +0530

    initial commit rupa

---
+++ rupa/src/rupa/ListClass.java
@@ -0,0 +1,17 @@
+package rupa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      List<Integer> id = new ArrayList<>();
+      for(int i=0 ; i<=10 ;i++){
+    	  id.add(i);
+      }
+      System.out.println(id);
+	}
+
+}
